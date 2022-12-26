5cdcc0584d47e7d49e9a5c55f10281859ea77248
awesome-mm <108213679+awesome-mm@users.noreply.github.com>
2022-12-26 22:44:52 +0900

    기업형 포트폴리오
    jusung_engineering 기업형

---
+++ jusung_engineering/member/check_id.php
@@ -0,0 +1,43 @@
+﻿<meta charset="utf-8">
+<?
+   
+   @extract($_POST);
+   @extract($_GET);
+   @extract($_SESSION);
+
+   //$id = 'a';
+  
+    if(!$id) 
+   {
+      echo("아이디를 입력하세요.");
+
+   } else if(strpos($id, ' ') !== false)
+   {
+       echo "<span>공백을 포함하지 않은 아이디를 입력하세요.</span>";
+      
+   } else {
+      include "../lib/dbconn.php";
+ 
+      $sql = "select * from member where id='$id' ";
+
+      $result = mysql_query($sql, $connect);
+      $num_record = mysql_num_rows($result);
+
+
+     
+      if ($num_record)
+      {
+       
+         echo "<span style='color:red'>다른 아이디를 사용하세요.</span>";
+      }
+      else
+      {
+         echo "<span style='color:green'>사용가능한 아이디입니다.</span>";
+      }
+    //data라는 매개변수에 echo의 문자열이 return되서 만들어지게 되어있다.
+ 
+      mysql_close();
+   }
+
+?>
+
